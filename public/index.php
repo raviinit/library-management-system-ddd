5ef13f3e61da7bf84f49d95359e9462be9a5898a
raviinit <36915695+raviinit@users.noreply.github.com>
2024-08-06 15:31:46 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,7 +7,6 @@ header('Access-Control-Allow-Methods: POST');
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
-$pdo = require_once '../config/database.php';
 
 if ($uri[1] !== 'api') {
     echo 'Website you are looking for is RESTRICTED.';
